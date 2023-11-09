3a47b66f8a9c3b64ed87a6dc879a9f98cbc95b68
Milos Novakovic <milos.novakovic.1337@gmail.com>
2023-11-09 10:00:46 +0100

    Refactoring Import Statements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import React from 'react'
-import { Box, Table } from '@radix-ui/themes'
-import Link from '../components/Link';
 import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
+import { Box, Table } from '@radix-ui/themes';
+import { IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   
   return (
     <Box>
