9e7e513e6216cc5a18ae9b52e93e45651c15ea48
Milos Novakovic <milos.novakovic.1337@gmail.com>
2023-11-09 08:39:45 +0100

    Add Markdown Visibility

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
+import ReactMarkdown from 'react-markdown';
 
 
 interface Props {
@@ -23,8 +24,8 @@ const IssueDetailPage = async ({ params }:  Props) => {
               <IssueStatusBadge status={selectedIssue.status} />
               <Text>{selectedIssue.createdAt.toDateString()}</Text>
           </Flex>
-          <Card>
-              <p>{selectedIssue.description}</p>
+          <Card className="prose" mt="4">
+              <ReactMarkdown>{selectedIssue.description}</ReactMarkdown>
           </Card>
       </div>
   )
