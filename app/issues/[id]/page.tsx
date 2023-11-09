ef8de48761ffec9a91c780ad0feb6a15adbdbcdc
Milos Novakovic <milos.novakovic.1337@gmail.com>
2023-11-09 10:28:16 +0100

    Refactor the Details Page (SRP)

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,35 +1,30 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
-import delay from 'delay';
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import React from 'react'
-import ReactMarkdown from 'react-markdown';
-
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface Props {
-    params: { id: string}
+  params: { id: string }
 }
 
-const IssueDetailPage = async ({ params }:  Props) => {
-  
+const IssueDetailPage = async ({ params }: Props) => {
+
   const selectedIssue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id)}
+    where: { id: parseInt(params.id) }
   });
 
   if (!selectedIssue) notFound();
-  
+
   return (
-      <div>
-          <Heading>{selectedIssue.title}</Heading>
-          <Flex className="space-x-3" my="2">
-              <IssueStatusBadge status={selectedIssue.status} />
-              <Text>{selectedIssue.createdAt.toDateString()}</Text>
-          </Flex>
-          <Card className="prose" mt="4">
-              <ReactMarkdown>{selectedIssue.description}</ReactMarkdown>
-          </Card>
-      </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={selectedIssue} />
+      </Box>
+      <Box>
+        <EditIssueButton issueId={selectedIssue.id} />
+      </Box>
+    </Grid>
   )
 }
 
